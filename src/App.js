71b939eff06bcb0e2a9ed107cd78ac873edd155e
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-03-07 14:14:50 -0800

    create new button component with style-components

--- src/App.js
+++ src/App.js
@@ -9,18 +9,20 @@ import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
 
-function App() {
-	return (
-		<React.Fragment>
-			<Navbar />
-			<Switch>
-				<Route exact path="/" component={ProductList} />
-				<Route path="/details" component={Details} />
-				<Route path="/cart" component={Cart} />
-				<Route component={Default} />
-			</Switch>
-		</React.Fragment>
-	);
+class App extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<Navbar />
+				<Switch>
+					<Route exact path="/" component={ProductList} />
+					<Route path="/details" component={Details} />
+					<Route path="/cart" component={Cart} />
+					<Route component={Default} />
+				</Switch>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
