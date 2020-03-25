500bb14a0f956f197cc513298590f501aaa8f53a
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-03-25 15:05:18 -0700

    update modal

--- src/context.js
+++ src/context.js
@@ -11,7 +11,10 @@ class ProductProvider extends Component {
 		detailProduct: detailProduct,
 		cart: [],
 		modalOpen: true,
-		modalProduct: detailProduct
+		modalProduct: detailProduct,
+		cartSubTotal: 0,
+		cartTax: 0,
+		cartTotal: 0
 	};
 	componentDidMount() {
 		this.setProducts();
@@ -69,6 +72,21 @@ class ProductProvider extends Component {
 		});
 	};
 
+	increment = (id) => {
+		console.log('this is the increment method');
+	};
+
+	decrement = (id) => {
+		console.log('this is the decrement method');
+	};
+	removeItem = (id) => {
+		console.log('item removed');
+	};
+
+	clearCart = () => {
+		console.log('cart was cleared!');
+	};
+
 	render() {
 		return (
 			<ProductContext.Provider
@@ -77,7 +95,11 @@ class ProductProvider extends Component {
 					handleDetail: this.handleDetail,
 					addToCart: this.addToCart,
 					openModal: this.openModal,
-					closeModal: this.closeModal
+					closeModal: this.closeModal,
+					increment: this.increment,
+					decrement: this.decrement,
+					removeItem: this.removeItem,
+					clearCart: this.clearCart
 				}}
 			>
 				{this.props.children}
