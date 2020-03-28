2adb747e383b0a1c3b3c74dbd78ef39ac794eb32
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-03-27 21:40:28 -0700

    update company name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ export default class Navar extends Component {
 				<ul className="navbar-nav align-items-center">
 					<li className="nav-item ml-5">
 						<Link to="/" className="nav-link">
-							products
+							Fortress
 						</Link>
 					</li>
 				</ul>
